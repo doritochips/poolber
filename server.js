74e4c4ea3adf906b130aa67f50e8e42b96910e86
Elvis Sun <elvissunlihe@gmail.com>
2016-11-15 13:20:39 -0500

    added mongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
-var app = express();
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var morgan = require('morgan')
+
+
+
+
+// middleware
+var app = express();
+
 
 app.use(express.static("public"));
 app.use(morgan('dev'));
+
+
+app.use(session({
+    secret: 'foo',
+    saveUninitialized: false, // don't create session until something stored 
+    resave: false, //don't save session if unmodified 
+    store: new MongoStore({
+        url: 'mongodb://dev:databasepassword@ds019856.mlab.com:19856/london2toronto'
+    })
+}));
+
 
 app.get('/', function(req, res){	
 	console.log("reaching: /");
