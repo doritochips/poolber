c0c755bbd518bbe7e07ec8b1bba2c344875f5011
Elvis Sun <elvissunlihe@gmail.com>
2016-11-15 13:01:20 -0500

    added more dependency
    1. run with `nodemon server.js` so you don't have to restart manually
    2. added `Procfile` and `forever` to deploy in the future
    3. added `morgan` for logging
    4. added `mongoose` for integration with mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan')
 
 app.use(express.static("public"));
+app.use(morgan('dev'));
+
 app.get('/', function(req, res){	
 	console.log("reaching: /");
 });
