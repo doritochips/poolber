e0100bd50b753554e87c5e29935fc33b3ad120a7
elvisun <elvissunlihe@gmail.com>
2016-11-18 15:30:39 -0500

    update user model

--- models/user.model.server.js
+++ models/user.model.server.js
@@ -1,12 +1,126 @@
 var mongoose = require('mongoose')
+var crypto = require('crypto')
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-    title:  String,
-    date: {
-    	type: Date, 
-    	default: Date.now
-    }
+/**
+ * A Validation function for local strategy properties
+ */
+var validateLocalStrategyProperty = function (property) {
+  return ((this.provider !== 'local' && !this.updated) || property.length);
+};
+
+/**
+ * A Validation function for local strategy email
+ */
+var validateLocalStrategyEmail = function (email) {
+  return ((this.provider !== 'local' && !this.updated) || validator.isEmail(email));
+};
+
+/**
+ * User Schema
+ */
+var UserSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your last name']
+  },
+  displayName: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
+  },
+  username: {
+    type: String,
+    unique: 'Username already exists',
+    required: 'Please fill in a username',
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  salt: {
+    type: String
+  },
+  profileImageURL: {
+    type: String,
+    default: 'public/client/img/profile/default.png'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  /* For reset password */
+  resetPasswordToken: {
+    type: String
+  },
+  resetPasswordExpires: {
+    type: Date
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+/**
+ * Hook a pre save method to hash the password
+ */
+UserSchema.pre('save', function (next) {
+  if (this.password && this.isModified('password')) {
+    this.salt = crypto.randomBytes(16).toString('base64');
+    this.password = this.hashPassword(this.password);
+  }
+
+  next();
+});
+
+/**
+ * Hook a pre validate method to test the local password
+ */
+UserSchema.pre('validate', function (next) {
+  if (this.provider === 'local' && this.password && this.isModified('password')) {
+    var result = owasp.test(this.password);
+    if (result.errors.length) {
+      var error = result.errors.join(' ');
+      this.invalidate('password', error);
+    }
+  }
+
+  next();
+});
+
+/**
+ * Create instance method for hashing a password
+ */
+UserSchema.methods.hashPassword = function (password) {
+  if (this.salt && password) {
+    return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64).toString('base64');
+  } else {
+    return password;
+  }
+};
+
+/**
+ * Create instance method for authenticating user
+ */
+UserSchema.methods.authenticate = function (password) {
+  return this.password === this.hashPassword(password);
+};
+
+mongoose.model('User', UserSchema);
+
+
+module.exports = mongoose.model('User', UserSchema);
