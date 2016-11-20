8c84ef401937920dc0ed95b16782ae1f3fa29524
elvisun <elvissunlihe@gmail.com>
2016-11-20 11:42:02 -0500

    get ride detail api

--- controllers/ride.controller.server.js
+++ controllers/ride.controller.server.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 var Ride = require('../models/ride.model.server.js');
 var User = require('../models/user.model.server.js');
+var mongoose = require("mongoose");
 
 exports.post = function(req, res) {
 	var newRide = new Ride(req.body);
@@ -17,7 +18,26 @@ exports.post = function(req, res) {
 };
 
 exports.read = function(req,res) {
-    res.json(req.article);
+    var id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({
+            message: 'ride id is invalid'
+        })
+    }
+    //populate with username
+    Ride.findById(id).exec(function(err, ride){
+        if (err){
+            return res.status(400).send(err);
+        }
+        else if (!ride){
+            return res.status(404).send({
+                message: 'No ride has been found'
+            });
+        }else{
+            res.json(ride);
+        }
+        
+    });
 }
 
 exports.update = function(req,res) {
--- routes/ride.routes.server.js
+++ routes/ride.routes.server.js
@@ -5,4 +5,5 @@ var rides = require('../controllers/ride.controller.server.js');
 module.exports = function(app) {
 	app.post('/api/ride', rides.post);
 	app.get('/api/rides', rides.list);
+	app.get('/api/ride/:id', rides.read);
 };
