2bda80b466fb54d2ca63d03d651982c1a35d96e2
David Nishikino <yuchenzhou123@gmail.com>
2016-11-20 22:16:56 -0500

    fix select

--- app/module/controller/post-ride.controller.js
+++ app/module/controller/post-ride.controller.js
@@ -1,13 +1,14 @@
 dash.controller("postRideCtrl", ["$http", "$scope", function($http, $scope){
-	$scope.form = {		
-	};	
-	$scope.cities = ["London", "Waterloo", "Kitchener", "Toronto"];
+	// init form
+	$scope.form = {				
+	};
+
+	$scope.cities = ["Select a city", "London", "Waterloo", "Kitchener", "Toronto"];
 	
 	$scope.popup = {
 		opened:false
 	};
 	$scope.open = function(){
-		document.getElementById("datepicker").focus();
 		$scope.popup.opened  = true;		
 	};
 	
