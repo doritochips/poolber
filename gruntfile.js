3d985e51ee2f87866ff6b5fd4f4fd461fe2e4357
elvisun <elvissunlihe@gmail.com>
2016-12-09 14:15:13 -0500

    init gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var path = require('path');
+
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
