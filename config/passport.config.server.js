c55fedac14d592d56621cd3af20972beb9938e31
elvisun <elvissunlihe@gmail.com>
2016-12-16 16:28:23 -0500

    2

--- config/passport.config.server.js
+++ config/passport.config.server.js
@@ -77,7 +77,7 @@ module.exports = function (app, db) {
 	var Wechat = new WechatStrategy({
 		appID: 'wxfe8d08cc30586362',						//'wx02d33dd17d1d0461',
 		appSecret: '3b9b6424f581913f22417f305abf6960',			//'d3361be9785345690c17213176415cf2',
-		callbackURL: '/api/auth/wechat/callback',
+		callbackURL: 'http://poolberwechat.herokuapp.com/api/auth/wechat/callback',
 		scope: 'snsapi_base',
 		state: '123'
 	},
